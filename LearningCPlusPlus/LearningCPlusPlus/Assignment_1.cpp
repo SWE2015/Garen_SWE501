4a3954c5f5a716cca8a2541c25edd9569f6a5c0c
Garen Yöndem <garen@yondem.com>
2015-10-18 23:21:54 +0300

    Assignment_1 update 1

--- LearningCPlusPlus/LearningCPlusPlus/Assignment_1.cpp
+++ LearningCPlusPlus/LearningCPlusPlus/Assignment_1.cpp
@@ -2,7 +2,7 @@
 #include <windows.h>
 using namespace std;
 
-double calcCompoundAmount(double *principal, double *interestRate, int *timesPerYear, double *years) {
+double calcCompoundInterest(double *principal, double *interestRate, int *timesPerYear, double *years) {
 	//formula S=P(1 + r/n)^nt
 	return	*principal * (pow((1 + (*interestRate / *timesPerYear)), (*timesPerYear * *years)));
 }
@@ -12,22 +12,24 @@ void main() {
 	double interestRate;
 	int timesPerYear;
 	double years;
+	double amount;
 
-	cout << "Welcome to compund amount calculator" << endl;
-	cout << "Please answer the questions below" << endl << endl;
+	cout << "Welcome to compound interest calculator" << endl;
+	cout << "Please answer the questions below and then press \"enter\" to proceed to next step" << endl << endl;
 
-	cout << "How much is your principal amount : " << endl;
+	cout << "How much is your principal amount: " << endl;
 	cin >> principal;
-	cout << "Interest rate : " << endl;
+	cout << "Interest rate: " << endl;
 	cin >> interestRate;
 	interestRate = interestRate / 100;
-	cout << "Number of payment payments you wish to make per year : " << endl;
+	cout << "Number of payments you wish to make each year: " << endl;
 	cin >> timesPerYear;
-	cout << "Due : " << endl;
+	cout << "Due: " << endl;
 	cin >> years;
 
-	principal = calcCompoundAmount(&principal, &interestRate, &timesPerYear, &years);
-	cout << "You final payment will be: " << principal;
+	amount = calcCompoundInterest(&principal, &interestRate, &timesPerYear, &years);
+	cout << "You final payment will be: " << amount << endl;
+	cout << "You will earn: " << amount - principal;
 
 	Sleep(1000000);
 }
